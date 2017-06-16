f37320bf2769bde9ff9d4092fda8278170a1f1d8
Flying Unicorn in SF <FlyingUnicornSF@users.noreply.github.com>
2017-06-15 17:31:43 -0700

    trying it without using Turtle class

--- flower.py
+++ flower.py
@@ -4,47 +4,46 @@ import turtle
 window = turtle.Screen()
 window.bgcolor('light blue')
 
-def draw_triangle(triangle, length):
+def draw_triangle(length):
     j = 0
     while j < 3:
-        triangle.forward(length)
-        triangle.right(120)
+        turtle.forward(length)
+        turtle.right(120)
         j = j + 1
 
-def flower_layer(flower2, angle, length):
+def flower_layer(angle, length):
     i = 0
     repeat = int(360 /angle)
     while i < repeat:
         draw_triangle(flower2, length)
-        flower2.right(angle)
+        turtle.right(angle)
         i = i + 1
 
-def draw_flower(flower, angle, layers):
+def draw_flower(angle, layers):
     #main functin that defines instance of Turtle class
     #and define shape of turtle
-    flower = turtle.Turtle()
-    flower.shape('turtle')
-    flower.color('purple','pink')
-    flower.speed(10)
+    turtle.shape('turtle')
+    turtle.color('purple','pink')
+    turtle.speed(10)
     #repeat to layer flower
     k = 0
     length = 100
     while k < layers:
-        flower_layer(flower, angle, length)
+        flower_layer(angle, length)
         k = k + 1
         length = length + 30
     #then move the turtle down to draw stem
-    flower.right(90)
-    flower.color("dark green")
-    flower.forward(300)
-    flower.right(155)
-    flower.circle(150, 60)
-    flower.left(120)
-    flower.circle(150, 120)
-    flower.left(120)
-    flower.circle(150, 60)
+    turtle.right(90)
+    turtle.color("dark green")
+    turtle.forward(300)
+    turtle.right(155)
+    turtle.circle(150, 60)
+    turtle.left(120)
+    turtle.circle(150, 120)
+    turtle.left(120)
+    turtle.circle(150, 60)
 
-draw_flower('babe', 15, 3)
+draw_flower(15, 3)
 
 
 
